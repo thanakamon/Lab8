fb7a0a3a741634acdff0fce29a58c770b09c19cc
thanakamon <thanakamonmay@gmail.com>
2018-11-02 16:10:26 +0700

    3

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,26 +6,22 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            int i;
             int[] data = new int[10];
-
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
-
-
-            for(i=0;i<10;i++)
-
-            Console.WriteLine(data[i]);
-            
+            Student[] student = new Student[31];
+            Console.WriteLine(data.Length);
+            Console.WriteLine(student.Length);
             Console.ReadLine();
+        }
+        class Student     // nested class
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
         }
     }
 }
+
+
