eb058c4524054028a2c96cf61030063f1d52cb0b
thanakamon <thanakamonmay@gmail.com>
2018-11-02 15:33:59 +0700

    2

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,14 +6,24 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
+            int i;
             int[] data = new int[10];
-            
+
+            data[0] = 0;
             data[1] = 1;
+            data[2] = 2;
+            data[3] = 3;
+            data[4] = 4;
+            data[5] = 5;
+            data[6] = 6;
+            data[7] = 7;
+            data[8] = 8;
             data[9] = 9;
-           
-            
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[9]);
+
+
+            for(i=0;i<10;i++)
+
+            Console.WriteLine(data[i]);
             
             Console.ReadLine();
         }
