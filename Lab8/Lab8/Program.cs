d9d026add8bd5de9f974cbf9b8ccc3fecc0acd48
thanakamon <thanakamonmay@gmail.com>
2018-11-04 14:22:17 +0700

    12*12

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -10,28 +10,38 @@ namespace MultidimensionalArray
     {
         static void Main(string[] args)
         {
-            int[,] multiplyTable = new int[13, 13];
-
-            for (int a = 1; a <= 12; a++)
+            int[,] multiplyTable = new int[12, 12];
+            int a, b;
+            for (a = 0, b = 1;a < 13 && b < 13;a++,b++)
             {
-
-                for (int i = 1; i <= 12; i++) 
-                {
-                    Console.WriteLine(multiplyTable[a, i] = a * i);
-                    for (int row = 0; row < multiplyTable.GetLength(0); row++)
-                    {
-                        for (int col = 0; col < multiplyTable.GetLength(1); col++)
-                {
-                Console.Write("{0,5}", multiplyTable[row, col]);
+            multiplyTable[0, a] = b * 1;
+            multiplyTable[1, a] = b * 2;
+            multiplyTable[2, a] = b * 3;
+            multiplyTable[3, a] = b * 4;
+            multiplyTable[4, a] = b * 5;
+            multiplyTable[5, a] = b * 6;
+            multiplyTable[6, a] = b * 7;
+            multiplyTable[7, a] = b * 8;
+            multiplyTable[8, a] = b * 9;
+            multiplyTable[9, a] = b * 10;
+            multiplyTable[10, a] = b * 11;
+            multiplyTable[11, a] = b * 12;
             }
-            Console.WriteLine();
-        }
-        Console.ReadLine();
-
-                }
-}
+            
             
 
+
+            for (int row = 0; row < multiplyTable.GetLength(0); row++)
+            {
+                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                {
+                    Console.Write("{0,5}", multiplyTable[row, col]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
+
+
