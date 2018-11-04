5b10b1917e831fcf5e8dca12a21c71dd6dcbb3fb
thanakamon <thanakamonmay@gmail.com>
2018-11-04 16:41:20 +0700

    remove dic

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -8,27 +8,32 @@ namespace DictionaryDemo
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> dict1 = new Dictionary<string, int>();
-            dict1.Add("One", 1);
-            dict1.Add("Two", 2);
-            dict1.Add("Three", 3);
-            dict1.Add("Four", 4);
-            dict1.Add("Five", 5);
-            dict1.Add("Six", 6);
-            dict1.Add("Seven", 7);
-            dict1.Add("Eight", 8);
-            dict1.Add("Nine", 9);
-            dict1.Add("Ten", 10);
-            if (dict1.ContainsKey("One"))
+            Dictionary<string, string> country = new Dictionary<string, string>();
+            country.Add("AF", "Afghanistan");
+            country.Add("AL", "Albania");
+            country.Add("AS", "American Samoa");
+            country.Add("AD", "Andorra");
+            country.Add("AM", "Armenia");
+            country.Add("AW", "Aruba");
+            country.Add("AU", "Australia");
+            country.Add("AT", "Austria");
+            country.Add("AZ", "Azerbaijan");
+            foreach (var item in country)
             {
-                int value = dict1["One"];
-                Console.WriteLine(value);
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
             }
-            if (dict1.ContainsKey("Eleven"))
+            Console.WriteLine("Count of items = {0}", country.Count);
+            Console.WriteLine("\n** Direct access to value by key **");
+            country["AU"] = "AUSTRALIA";
+            Console.WriteLine("AU = " + country["AU"]);
+
+            Console.WriteLine("\n** Remove by key **");
+            country.Remove("AU");
+            foreach (var item in country)
             {
-                int value = dict1["Eleven"];
-                Console.WriteLine(value);
+                Console.WriteLine("{0,2} = {1}", item.Key, item.Value);
             }
+            Console.WriteLine("Count of items = {0}", country.Count);
             Console.ReadLine();
         }
     }
