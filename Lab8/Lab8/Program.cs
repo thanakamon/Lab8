337a6b972857b60ef1334819d98e657c01c19af8
thanakamon <thanakamonmay@gmail.com>
2018-11-04 17:18:58 +0700

    รอed

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -15,15 +15,10 @@ namespace HashTableDemo
             weeks.Add("5", "Thursday");
             weeks.Add("6", "Friday");
             weeks.Add("7", "Saturday");
-
-            // Method ContainKey()
-            Console.WriteLine("The  key element \"8\" is contain in the Hashtable weeks: " + weeks.ContainsKey("8"));
-
-            // Method ContainValue()
-            Console.WriteLine("The  key element \"Wednesday\" is contain in theHashtable weeks: " + weeks.ContainsValue("Wednesday"));
-
-            Console.ReadLine();
+            DictionaryEntry day in weeks
+            Console.WriteLine("..");
+            day.Key = string();
+            Console.WriteLine(day.Value);
         }
     }
 }
-
