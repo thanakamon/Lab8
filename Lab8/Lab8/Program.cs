c2fc680b4e3b19ef027096abcc97eddac23598d1
thanakamon <thanakamonmay@gmail.com>
2018-11-04 17:03:51 +0700

    Hashtable weeks

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
-namespace DictionaryDemo
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dict1 = new Dictionary<string, string>();
-            dict1.Add("txt", "Notepad.exe");
-            dict1.Add("bmp", "paint.exe");
-            dict1.Add("rtf", "wordpad.exe");
-            dict1.Add("pdf", "acrobat.exe");
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "SunDay");
+            weeks.Add("2", "MonDay");
+            weeks.Add("3", "TuesDay");
+            weeks.Add("4", "WednesDay");
+            weeks.Add("5", "ThursDay");
+            weeks.Add("6", "FriDay");
+            weeks.Add("7", "SaturDay");
 
-            foreach (var item in dict1)
+            foreach (DictionaryEntry day in weeks)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(day.Key + "   -   " + day.Value);
             }
-
-            foreach (var item in dict1)
-            {
-                Console.WriteLine("key = {0}, value = {1}", item.Key, item.Value);
-            }
-
             Console.ReadLine();
         }
     }
