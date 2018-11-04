28d5b3f8ddfc990d1bdc6d516ff5f0c9659a775d
thanakamon <thanakamonmay@gmail.com>
2018-11-04 14:54:34 +0700

    jag

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,47 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MultidimensionalArray
+namespace ArrayOfArray
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] multiplyTable = new int[12, 12];
-            int a, b;
-            for (a = 0, b = 1;a < 13 && b < 13;a++,b++)
+            int[][] jagged = new int[7][];
+            int count = 0;
+            for (int row = 0; row < jagged.GetLength(0); ++row)
             {
-            multiplyTable[0, a] = b * 1;
-            multiplyTable[1, a] = b * 2;
-            multiplyTable[2, a] = b * 3;
-            multiplyTable[3, a] = b * 4;
-            multiplyTable[4, a] = b * 5;
-            multiplyTable[5, a] = b * 6;
-            multiplyTable[6, a] = b * 7;
-            multiplyTable[7, a] = b * 8;
-            multiplyTable[8, a] = b * 9;
-            multiplyTable[9, a] = b * 10;
-            multiplyTable[10, a] = b * 11;
-            multiplyTable[11, a] = b * 12;
-            }
-            
-            
-
-
-            for (int row = 0; row < multiplyTable.GetLength(0); row++)
-            {
-                for (int col = 0; col < multiplyTable.GetLength(1); col++)
+                Console.Write("\nRow {0}:", row);
+                jagged[row] = new int[row + 1];
+                for (int index = 0; index < row + 1; ++index)
                 {
-                    Console.Write("{0,5}", multiplyTable[row, col]);
+                    ++count;
+                    jagged[row][index] = count;
+                    Console.Write("{0}", count);
                 }
-                Console.WriteLine();
+            }
+            Console.WriteLine("\n\nTotals");
+            for (int row = 0; row < jagged.GetLength(0); ++row)
+            {
+                int total = 0;
+                for (int index = 0; index < jagged[row].GetLength(0); ++index)
+                {
+                    total += jagged[row][index];
+                }
+                Console.Write("\nRow {0}: {1}", row, total);
             }
             Console.ReadLine();
         }
     }
 }
-
-
