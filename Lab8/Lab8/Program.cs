6bcc87ee3acc7306307a0e4c8c7c4483de4eec23
thanakamon <thanakamonmay@gmail.com>
2018-11-04 16:12:43 +0700

    deleteq

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -8,24 +8,14 @@ namespace QueueDemo
         static void Main(string[] args)
         {
             Queue<string> queue1 = new Queue<string>();
-            queue1.Enqueue("Red");
-            queue1.Enqueue("Green");
-            queue1.Enqueue("Blue");
-            queue1.Enqueue("Orange");
-            queue1.Enqueue("Yellow");
-            Console.WriteLine("The elements in the queue are:");
-            foreach (string s in queue1)
-            {
-                Console.WriteLine(s);
-            }
-            Console.WriteLine("The element Red is contain in the queue:"
- + queue1.Contains("Red"));
-            Console.WriteLine("The element Black is contain in the queue:"
- + queue1.Contains("Black"));
-            Console.WriteLine("The element Purple is contain in the queue:"
- + queue1.Contains("Purple"));
-            Console.WriteLine("The element Green is contain in the queue:"
- + queue1.Contains("Green"));
+            queue1.Enqueue("RED");
+            queue1.Enqueue("BLUE");
+            queue1.Enqueue("YELLOW");
+            queue1.Enqueue("GREEN");
+
+            Console.WriteLine("The elements in the queue are:" + queue1.Count);
+            queue1.Clear();
+            Console.WriteLine("The elements in the queue are after the clearmethod: " + queue1.Count);
             Console.ReadLine();
         }
     }
