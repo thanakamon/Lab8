e8a1a25521584d66a86d48c3dec96f046a725838
thanakamon <thanakamonmay@gmail.com>
2018-11-04 11:46:16 +0700

    4

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,14 +6,29 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            double[] data = { 3.1, 3.3, 3.2, 3.5, 3.4, 3.5, 3.2, 3.2  };
-           
-            for (int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine(data[i]);
-            }
-            Console.WriteLine("GPA=" + (data[0] + data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7])/8);
-            Console.ReadLine();
+            int[] data = { 0,0, 3, 3, 6, 1, 4, 6, 2,5,0,3,5  };
+            Console.WriteLine("Date");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Month");
+            int a = int.Parse(Console.ReadLine());
+            int s = i + a -1;
+            int b = s % 7;
+            Console.WriteLine(b);
+            if (b == 0) 
+            Console.WriteLine("{0}//{1}//2560(Sunday)", i,a);
+            if (b == 1) 
+            Console.WriteLine("{0}//{1}//2560(Monday)", i, a);
+            if (b == 2) 
+            Console.WriteLine("{0}//{1}//2560(Tuesday)", i, a);
+            if (b == 3) 
+            Console.WriteLine("{0}//{1}//2560(Wednesday)", i, a);
+            if (b == 4) 
+            Console.WriteLine("{0}//{1}//2560(Thursday)", i, a);
+            if (b == 5) 
+            Console.WriteLine("{0}//{1}//2560(Friday)", i, a);
+            if (b == 6) 
+            Console.WriteLine("{0}//{1}//2560(Saturday)", i, a);
+
         }
     }
 }
