ec339a489a5a9e5a34acd387deeaf697abe41928
thanakamon <thanakamonmay@gmail.com>
2018-11-04 12:15:02 +0700

    8

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,36 +6,19 @@ namespace ConsoleAppArray
     {
         static void Main(string[] args)
         {
-            string[] array1 = { "cat", "dog", "carrot", "bird" };
-
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีค่าตามกำหนด 
-            //
-            string value1 = Array.Find(array1,
-              element => element.StartsWith("car",
-              StringComparison.Ordinal));
-
-            string value2 = Array.Find(array1,
-              element => element.StartsWith("fish",
-              StringComparison.Ordinal));
-
-            //
-            // ค้นหาสมาชิกตัวแรกที่มีความยาว string ตามกำหนด
-            //
-            string value3 = Array.Find(array1,
-                element => element.Length == 3);
-
-            //
-            // ค้นหา string ที่มีความยาวไม่เกินค่าที่กำหนด
-            //
+            string[] array1 = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            string[] array5 = Array.FindAll(array1,
+            element => element.Length <= 3);
             string[] array2 = Array.FindAll(array1,
-                element => element.Length <= 4);
-
-            Console.WriteLine(value1);
-            Console.WriteLine(value2);
-            Console.WriteLine(value3);
+            element => element.Length >= 10);
+            string[] array3 = Array.FindAll(array1,
+            element => element.StartsWith("J", 
+            StringComparison.Ordinal));
+            Console.WriteLine(string.Join(",", array5));
             Console.WriteLine(string.Join(",", array2));
-            Console.ReadLine();
+            Console.WriteLine(string.Join(",", array3));
+
+
         }
     }
 }
