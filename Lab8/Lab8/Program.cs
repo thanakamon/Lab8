ea8b2a51db36c0874419dcca52f7152a81700cd2
thanakamon <thanakamonmay@gmail.com>
2018-11-05 12:29:51 +0700

    Clear()

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,56 +6,50 @@ public class SamplesArrayList
     public static void Main()
     {
 
-        // Creates and initializes a new ArrayList using Insert instead of Add.
+        // Creates and initializes a new ArrayList.
         ArrayList myAL = new ArrayList();
-        myAL.Insert(0, "The");
-        myAL.Insert(1, "fox");
-        myAL.Insert(2, "jumps");
-        myAL.Insert(3, "over");
-        myAL.Insert(4, "the");
-        myAL.Insert(5, "dog");
+        myAL.Add("The");
+        myAL.Add("quick");
+        myAL.Add("brown");
+        myAL.Add("fox");
+        myAL.Add("jumps");
 
-        // Creates and initializes a new Queue.
-        Queue myQueue = new Queue();
-        myQueue.Enqueue("quick");
-        myQueue.Enqueue("brown");
-
-        // Displays the ArrayList and the Queue.
-        Console.WriteLine("The ArrayList initially contains the following:");
-        PrintValues(myAL);
-        Console.WriteLine("The Queue initially contains the following:");
-        PrintValues(myQueue);
-
-        // Copies the Queue elements to the ArrayList at index 1.
-        myAL.InsertRange(1, myQueue);
-
-        // Displays the ArrayList.
-        Console.WriteLine("After adding the Queue, the ArrayList now contains:");
+        // Displays the count, capacity and values of the ArrayList.
+        Console.WriteLine("Initially,");
+        Console.WriteLine("   Count    : {0}", myAL.Count);
+        Console.WriteLine("   Capacity : {0}", myAL.Capacity);
+        Console.Write("   Values:");
         PrintValues(myAL);
 
-        // Search for "dog" and add "lazy" before it.
-        myAL.Insert(myAL.IndexOf("dog"), "lazy");
+        // Trim the ArrayList.
+        myAL.TrimToSize();
 
-        // Displays the ArrayList.
-        Console.WriteLine("After adding \"lazy\", the ArrayList now contains:");
+        // Displays the count, capacity and values of the ArrayList.
+        Console.WriteLine("After TrimToSize,");
+        Console.WriteLine("   Count    : {0}", myAL.Count);
+        Console.WriteLine("   Capacity : {0}", myAL.Capacity);
+        Console.Write("   Values:");
         PrintValues(myAL);
 
-        // Add "!!!" at the end.
-        myAL.Insert(myAL.Count, "!!!");
+        // Clear the ArrayList.
+        myAL.Clear();
 
-        // Displays the ArrayList.
-        Console.WriteLine("After adding \"!!!\", the ArrayList now contains:");
+        // Displays the count, capacity and values of the ArrayList.
+        Console.WriteLine("After Clear,");
+        Console.WriteLine("   Count    : {0}", myAL.Count);
+        Console.WriteLine("   Capacity : {0}", myAL.Capacity);
+        Console.Write("   Values:");
         PrintValues(myAL);
 
-        // Inserting an element beyond Count throws an exception.
-        try
-        {
-            myAL.Insert(myAL.Count + 1, "anystring");
-        }
-        catch (Exception myException)
-        {
-            Console.WriteLine("Exception: " + myException.ToString());
-        }
+        // Trim the ArrayList again.
+        myAL.TrimToSize();
+
+        // Displays the count, capacity and values of the ArrayList.
+        Console.WriteLine("After the second TrimToSize,");
+        Console.WriteLine("   Count    : {0}", myAL.Count);
+        Console.WriteLine("   Capacity : {0}", myAL.Capacity);
+        Console.Write("   Values:");
+        PrintValues(myAL);
     }
 
     public static void PrintValues(IEnumerable myList)
@@ -69,18 +63,20 @@ public class SamplesArrayList
 /* 
 This code produces the following output.
 
-The ArrayList initially contains the following:
-   The   fox   jumps   over   the   dog
-The Queue initially contains the following:
-   quick   brown
-After adding the Queue, the ArrayList now contains:
-   The   quick   brown   fox   jumps   over   the   dog
-After adding "lazy", the ArrayList now contains:
-   The   quick   brown   fox   jumps   over   the   lazy   dog
-After adding "!!!", the ArrayList now contains:
-   The   quick   brown   fox   jumps   over   the   lazy   dog   !!!
-Exception: System.ArgumentOutOfRangeException: Insertion index was out of range.  Must be non-negative and less than or equal to size.
-Parameter name: index
-   at System.Collections.ArrayList.Insert(Int32 index, Object value)
-   at SamplesArrayList.Main()
+Initially,
+   Count    : 5
+   Capacity : 16
+   Values:    The    quick    brown    fox    jumps
+After TrimToSize,
+   Count    : 5
+   Capacity : 5
+   Values:    The    quick    brown    fox    jumps
+After Clear,
+   Count    : 0
+   Capacity : 5
+   Values:
+After the second TrimToSize,
+   Count    : 0
+   Capacity : 16
+   Values:
 */
