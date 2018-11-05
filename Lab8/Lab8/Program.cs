c7635299ac77f77572769b7d278e2567662c7c51
thanakamon <thanakamonmay@gmail.com>
2018-11-05 12:14:04 +0700

    ● RemoveAt(int)

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 using System.Collections;
-
-namespace HashTableDemo
+public class SamplesArrayList
 {
-    class Program
+
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Hashtable weeks = new Hashtable();
-            weeks.Add("1", "Sunday");
-            weeks.Add("2", "Monday");
-            weeks.Add("3", "Tuesday");
-            weeks.Add("4", "Wednesday");
-            weeks.Add("5", "Thursday");
-            weeks.Add("6", "Friday");
-            weeks.Add("7", "Saturday");
-            DictionaryEntry day in weeks
-            Console.WriteLine("..");
-            day.Key = string();
-            Console.WriteLine(day.Value);
-        }
+
+        // Creates and initializes a new ArrayList.
+        ArrayList myAL = new ArrayList();
+        myAL.Add("The");
+        myAL.Add("quick");
+        myAL.Add("brown");
+        myAL.Add("fox");
+        myAL.Add("jumps");
+        myAL.Add("over");
+        myAL.Add("the");
+        myAL.Add("lazy");
+        myAL.Add("dog");
+
+        // Displays the ArrayList.
+        Console.WriteLine("The ArrayList initially contains the following:");
+        PrintValues(myAL);
+
+        // Removes the element containing "lazy".
+        myAL.Remove("lazy");
+
+        // Displays the current state of the ArrayList.
+        Console.WriteLine("After removing \"lazy\":");
+        PrintValues(myAL);
+
+        // Removes the element at index 5.
+        myAL.RemoveAt(5);
+
+        // Displays the current state of the ArrayList.
+        Console.WriteLine("After removing the element at index 5:");
+        PrintValues(myAL);
+
+        // Removes three elements starting at index 4.
+        myAL.RemoveRange(4, 3);
+
+        // Displays the current state of the ArrayList.
+        Console.WriteLine("After removing three elements starting at index 4:");
+        PrintValues(myAL);
     }
+
+    public static void PrintValues(IEnumerable myList)
+    {
+        foreach (Object obj in myList)
+            Console.Write("   {0}", obj);
+        Console.WriteLine();
+    }
+
 }
