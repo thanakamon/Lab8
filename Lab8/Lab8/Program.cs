5f167c93054dcc17802d70c29b89a5c7b175c1d4
thanakamon <thanakamonmay@gmail.com>
2018-11-05 12:24:21 +0700

    ● Insert(int, object)

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,42 +6,56 @@ public class SamplesArrayList
     public static void Main()
     {
 
-        // Creates and initializes a new ArrayList.
+        // Creates and initializes a new ArrayList using Insert instead of Add.
         ArrayList myAL = new ArrayList();
-        myAL.Add("The");
-        myAL.Add("quick");
-        myAL.Add("brown");
-        myAL.Add("fox");
-        myAL.Add("jumps");
-        myAL.Add("over");
-        myAL.Add("the");
-        myAL.Add("lazy");
-        myAL.Add("dog");
+        myAL.Insert(0, "The");
+        myAL.Insert(1, "fox");
+        myAL.Insert(2, "jumps");
+        myAL.Insert(3, "over");
+        myAL.Insert(4, "the");
+        myAL.Insert(5, "dog");
 
-        // Displays the ArrayList.
+        // Creates and initializes a new Queue.
+        Queue myQueue = new Queue();
+        myQueue.Enqueue("quick");
+        myQueue.Enqueue("brown");
+
+        // Displays the ArrayList and the Queue.
         Console.WriteLine("The ArrayList initially contains the following:");
         PrintValues(myAL);
+        Console.WriteLine("The Queue initially contains the following:");
+        PrintValues(myQueue);
 
-        // Removes the element containing "lazy".
-        myAL.Remove("lazy");
+        // Copies the Queue elements to the ArrayList at index 1.
+        myAL.InsertRange(1, myQueue);
 
-        // Displays the current state of the ArrayList.
-        Console.WriteLine("After removing \"lazy\":");
+        // Displays the ArrayList.
+        Console.WriteLine("After adding the Queue, the ArrayList now contains:");
         PrintValues(myAL);
 
-        // Removes the element at index 5.
-        myAL.RemoveAt(5);
+        // Search for "dog" and add "lazy" before it.
+        myAL.Insert(myAL.IndexOf("dog"), "lazy");
 
-        // Displays the current state of the ArrayList.
-        Console.WriteLine("After removing the element at index 5:");
+        // Displays the ArrayList.
+        Console.WriteLine("After adding \"lazy\", the ArrayList now contains:");
         PrintValues(myAL);
 
-        // Removes three elements starting at index 4.
-        myAL.RemoveRange(4, 3);
+        // Add "!!!" at the end.
+        myAL.Insert(myAL.Count, "!!!");
 
-        // Displays the current state of the ArrayList.
-        Console.WriteLine("After removing three elements starting at index 4:");
+        // Displays the ArrayList.
+        Console.WriteLine("After adding \"!!!\", the ArrayList now contains:");
         PrintValues(myAL);
+
+        // Inserting an element beyond Count throws an exception.
+        try
+        {
+            myAL.Insert(myAL.Count + 1, "anystring");
+        }
+        catch (Exception myException)
+        {
+            Console.WriteLine("Exception: " + myException.ToString());
+        }
     }
 
     public static void PrintValues(IEnumerable myList)
@@ -56,11 +70,17 @@ public class SamplesArrayList
 This code produces the following output.
 
 The ArrayList initially contains the following:
-   The   quick   brown   fox   jumps   over   the   lazy   dog
-After removing "lazy":
+   The   fox   jumps   over   the   dog
+The Queue initially contains the following:
+   quick   brown
+After adding the Queue, the ArrayList now contains:
    The   quick   brown   fox   jumps   over   the   dog
-After removing the element at index 5:
-   The   quick   brown   fox   jumps   the   dog
-After removing three elements starting at index 4:
-   The   quick   brown   fox
+After adding "lazy", the ArrayList now contains:
+   The   quick   brown   fox   jumps   over   the   lazy   dog
+After adding "!!!", the ArrayList now contains:
+   The   quick   brown   fox   jumps   over   the   lazy   dog   !!!
+Exception: System.ArgumentOutOfRangeException: Insertion index was out of range.  Must be non-negative and less than or equal to size.
+Parameter name: index
+   at System.Collections.ArrayList.Insert(Int32 index, Object value)
+   at SamplesArrayList.Main()
 */
