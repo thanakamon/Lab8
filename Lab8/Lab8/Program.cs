e910bd9b0a6efa1ccf2e8f776c006bbf8234a7b8
thanakamon <thanakamonmay@gmail.com>
2018-11-05 13:36:46 +0700

    sid

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 
-namespace DictionaryDemo
+namespace HashTableDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dict1 = new Dictionary<string, string>();
-            dict1.Add("th", "thailand");
+            Hashtable c = new Hashtable();
+            c.Add("60030007", "ging");
+            c.Add("60030014", "jimmy");
+            c.Add("60030015", "golf");
+            c.Add("60030016", "first");
+            c.Add("60030029", "aun");
+            c.Add("60030031", "may");
+            c.Add("600300", "meen");
+            c.Add("60030041", "o");
+            c.Add("60030049", "tar");
+            c.Add("60030089", "boat");
+            c.Add("60030098", "aomsin");
+            c.Add("60030112", "rut");
 
-
-            string value = dict1[Console.ReadLine()];
-            Console.WriteLine(value);
-
+            foreach (DictionaryEntry day in c)
+            {
+                Console.WriteLine(day.Key + "\n   " + day.Value);
+            }
+            Console.WriteLine("pls input sid: ");
+            Console.WriteLine("" + c.ContainsKey(Console.ReadLine()));
             Console.ReadLine();
         }
     }
 }
+
