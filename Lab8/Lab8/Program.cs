4a7aaf083ad3ade4b751b6b71851065eebc04bd7
thanakamon <thanakamonmay@gmail.com>
2018-11-05 13:37:01 +0700

    numsp

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -8,27 +8,12 @@ namespace HashTableDemo
         static void Main(string[] args)
         {
             Hashtable c = new Hashtable();
-            c.Add("60030007", "ging");
-            c.Add("60030014", "jimmy");
-            c.Add("60030015", "golf");
-            c.Add("60030016", "first");
-            c.Add("60030029", "aun");
-            c.Add("60030031", "may");
-            c.Add("600300", "meen");
-            c.Add("60030041", "o");
-            c.Add("60030049", "tar");
-            c.Add("60030089", "boat");
-            c.Add("60030098", "aomsin");
-            c.Add("60030112", "rut");
+            c.Add("72000", "suphan");
 
-            foreach (DictionaryEntry day in c)
-            {
-                Console.WriteLine(day.Key + "\n   " + day.Value);
-            }
-            Console.WriteLine("pls input sid: ");
+
+            Console.WriteLine("pls input num: ");
             Console.WriteLine("" + c.ContainsKey(Console.ReadLine()));
             Console.ReadLine();
         }
     }
 }
-
