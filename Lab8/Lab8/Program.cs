a111fefab3bf4ba27c21924f2a67b1141f104da4
thanakamon <thanakamonmay@gmail.com>
2018-11-05 12:36:40 +0700

    IndexOf

--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -6,66 +6,71 @@ public class SamplesArrayList
     public static void Main()
     {
 
-        // Creates and initializes the source ArrayList.
-        ArrayList mySourceList = new ArrayList();
-        mySourceList.Add("three");
-        mySourceList.Add("napping");
-        mySourceList.Add("cats");
-        mySourceList.Add("in");
-        mySourceList.Add("the");
-        mySourceList.Add("barn");
+        // Creates and initializes a new ArrayList with three elements of the same value.
+        ArrayList myAL = new ArrayList();
+        myAL.Add("the");
+        myAL.Add("quick");
+        myAL.Add("brown");
+        myAL.Add("fox");
+        myAL.Add("jumps");
+        myAL.Add("over");
+        myAL.Add("the");
+        myAL.Add("lazy");
+        myAL.Add("dog");
+        myAL.Add("in");
+        myAL.Add("the");
+        myAL.Add("barn");
 
-        // Creates and initializes the one-dimensional target Array.
-        String[] myTargetArray = new String[15];
-        myTargetArray[0] = "The";
-        myTargetArray[1] = "quick";
-        myTargetArray[2] = "brown";
-        myTargetArray[3] = "fox";
-        myTargetArray[4] = "jumps";
-        myTargetArray[5] = "over";
-        myTargetArray[6] = "the";
-        myTargetArray[7] = "lazy";
-        myTargetArray[8] = "dog";
+        // Displays the values of the ArrayList.
+        Console.WriteLine("The ArrayList contains the following values:");
+        PrintIndexAndValues(myAL);
 
-        // Displays the values of the target Array.
-        Console.WriteLine("The target Array contains the following (before and after copying):");
-        PrintValues(myTargetArray, ' ');
+        // Search for the first occurrence of the duplicated value.
+        String myString = "the";
+        int myIndex = myAL.IndexOf(myString);
+        Console.WriteLine("The first occurrence of \"{0}\" is at index {1}.", myString, myIndex);
 
-        // Copies the second element from the source ArrayList to the target Array starting at index 7.
-        mySourceList.CopyTo(1, myTargetArray, 7, 1);
+        // Search for the first occurrence of the duplicated value in the last section of the ArrayList.
+        myIndex = myAL.IndexOf(myString, 4);
+        Console.WriteLine("The first occurrence of \"{0}\" between index 4 and the end is at index {1}.", myString, myIndex);
 
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
+        // Search for the first occurrence of the duplicated value in a section of the ArrayList.
+        myIndex = myAL.IndexOf(myString, 6, 6);
+        Console.WriteLine("The first occurrence of \"{0}\" between index 6 and index 11 is at index {1}.", myString, myIndex);
 
-        // Copies the entire source ArrayList to the target Array starting at index 6.
-        mySourceList.CopyTo(myTargetArray, 6);
-
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
-
-        // Copies the entire source ArrayList to the target Array starting at index 0.
-        mySourceList.CopyTo(myTargetArray);
-
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
+        // Search for the first occurrence of the duplicated value in a small section at the end of the ArrayList.
+        myIndex = myAL.IndexOf(myString, 11);
+        Console.WriteLine("The first occurrence of \"{0}\" between index 11 and the end is at index {1}.", myString, myIndex);
     }
 
-    public static void PrintValues(String[] myArr, char mySeparator)
+    public static void PrintIndexAndValues(IEnumerable myList)
     {
-        for (int i = 0; i < myArr.Length; i++)
-            Console.Write("{0}{1}", mySeparator, myArr[i]);
+        int i = 0;
+        foreach (Object obj in myList)
+            Console.WriteLine("   [{0}]:    {1}", i++, obj);
         Console.WriteLine();
     }
 
 }
-
-
 /* 
-This code produces the following output.
+This code produces output similar to the following:
+ 
+The ArrayList contains the following values:
+   [0]:    the
+   [1]:    quick
+   [2]:    brown
+   [3]:    fox
+   [4]:    jumps
+   [5]:    over
+   [6]:    the
+   [7]:    lazy
+   [8]:    dog
+   [9]:    in
+   [10]:    the
+   [11]:    barn
 
-The target Array contains the following (before and after copying):
- The quick brown fox jumps over the lazy dog
- The quick brown fox jumps over the napping dog
- The quick brown fox jumps over three napping cats in the barn
- three napping cats in the barn three napping cats in the barn
+The first occurrence of "the" is at index 0.
+The first occurrence of "the" between index 4 and the end is at index 6.
+The first occurrence of "the" between index 6 and index 11 is at index 6.
+The first occurrence of "the" between index 11 and the end is at index -1.
 */
